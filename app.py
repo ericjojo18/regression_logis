b5f0776240cbaf7f500a0e209be9e47dd5fb5053
ericjojo18 <56933352+ericjojo18@users.noreply.github.com>
2021-12-07 17:51:31 +0000

    analyse

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, flash
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/post-predict', methods=['POST'])
+def predict():
+    for x in request.form.values():
+        if not x:
+            erreur = "Rensigner tout les champs"
+            return render_template('index.html', erreur=erreur)
+    input_valeur = [float(x) for x in request.form.values()]
+    tab_np = np.array(input_valeur)
+    tab_np = np.reshape(tab_np, (1, -1))
+
+    pred = model.predict(tab_np)
+    return render_template('index.html', predict=pred)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
